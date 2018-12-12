7c0cabba85a4ef3b34fff8acd92d2cb4f2da6d92
Tony <asantiago323@gmail.com>
2018-12-12 09:52:43 -0500

    uploaded files

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,39 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server.');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo: ', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    /*
+     * Insert documents into the Users Collection
+     * (name, age, location)
+     */
+
+    db.collection('Users').insertOne({
+        name: 'Anthony Santiago',
+        age: 34,
+        location: 'Virginia, USA'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user: ', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.close();
+});
