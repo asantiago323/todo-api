d869b6c5db27d8d0e8084139da5f51e00c4ee236
Anthony Santiago <Anthony.Santiago@caci.com>
2019-01-02 13:04:09 -0500

    Added deleteById and test cases

--- server/server.js
+++ server/server.js
@@ -55,6 +55,28 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // validate id
+    if (!ObjectID.isValid(id)) {
+        // stop and respond with 404
+        return res.status(404).send();
+    }
+
+    // findById
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            // return item or 404 if empty
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
