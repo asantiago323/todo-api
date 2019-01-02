b7aac50cd9a37eee110d900ea4029795605f87c5
Anthony Santiago <Anthony.Santiago@caci.com>
2019-01-02 11:44:54 -0500

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // setting promise type
-mongoose.connect('mongodb://localhost:27017/TodoApp'); // local mongodb
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // local mongodb
 
 module.exports = {mongoose};
